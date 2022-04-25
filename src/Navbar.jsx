a279c4e742071ea61041fdf2021e6db3eb8aa0f9
Siri2997 <104083917+Siri2997@users.noreply.github.com>
2022-04-25 16:41:46 +0530

    my code

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import { NavLink } from "react-router-dom";
 const Navbar=()=>{
+
+  const [show, setShow]=useState(false);
+
     return (
     <>
     <div className="container fluid nav_bg">
@@ -12,16 +15,19 @@ const Navbar=()=>{
           SSP Technical 
           </NavLink>
           <button className="navbar-toggler" 
-          type="button" data-bs-toggle="collapse" 
+          type="button" 
+          data-bs-toggle="collapse" 
           data-bs-target="#navbarSupportedContent" 
           aria-controls="navbarSupportedContent" 
           aria-expanded="false" 
-          aria-label="Toggle navigation">
+          aria-label="Toggle navigation"
+          onClick={()=>setShow(!show)}
+          >
+
           <span className ="navbar-toggler-icon">
           </span>
           </button>
-          <div className= "collapse navbar-collapse" 
-          id= "navbarSupportedContent" >
+          <div className= {`collapse navbar-collapse ${show ? "show" : ""}`}  >
            <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
           <li className="nav-item">
           <NavLink activeClassName = 'menu_active'
